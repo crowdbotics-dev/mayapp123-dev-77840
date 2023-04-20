fce0f805750dba8773d2d4cbe6b8435a9a771785
Crowdbotics <engineering@crowdbotics.com>
2023-04-20 05:55:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mayapp123_dev_77840/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mayapp123_dev_77840;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "mayapp123_dev_77840";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mayapp123-dev-77840",
+  url: "https://mayapp123-dev-77840.botics.co",
+  api: "https://mayapp123-dev-77840.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
